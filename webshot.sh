f2ec57e950e5eb42f8a6bee1a988da9018a7bdab
Saroj <wostz@localhost.localdomain>
2022-04-07 22:03:30 +0545

    Added subdomain finding and httprobe for alive domains

--- webshot.sh
+++ webshot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-urls=$1
+subfinder -dL $1 -o subdomains.txt | httprobe > alivedomains.txt
+rm -rf subdomains.txt
 mkdir shots
 while IFS= read line
 do
   screenshot=$(echo "$line" | sed 's/https\?:\/\///' | sed 's~/[a-z]*~~g')
   cutycapt --url=$line --out="shots/$screenshot".png --min-height=800
   echo "Screenshot taken for url: $line"
-done < "$urls"
+done < alivedomains.txt
